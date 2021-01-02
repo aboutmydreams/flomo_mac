16c7a1aa8c3f081d9df749d20307e3b3d80617a8
aboutmydreams <aboutmydreams@163.com>
2021-01-02 13:03:03 +0800

    :arrow_up: update flutter to 1.26.0

--- android/app/src/main/kotlin/com/example/flomo_mac/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flomo_mac
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import desktop_window
+import flutter_macos_webview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DesktopWindowPlugin.register(with: registry.registrar(forPlugin: "DesktopWindowPlugin"))
+  FlutterMacOSWebViewPlugin.register(with: registry.registrar(forPlugin: "FlutterMacOSWebViewPlugin"))
 }
